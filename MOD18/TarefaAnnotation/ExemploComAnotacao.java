5a64f3546ec63739e5d0fa7c4720ac020fe1a4a7
Douglas Candido <doug.candido2704@gmail.com>
2023-11-06 04:56:02 -0300

    Adicionado objeto que usa a anotação @Tabela, esse objeto é apenas um exemplo

---
+++ MOD18/TarefaAnnotation/ExemploComAnotacao.java
@@ -0,0 +1,8 @@
+
+@Tabela(nome = "ExemploComAnotacao")
+public class ExemploComAnotacao {
+	public static void main(String[] args) {
+		System.out.println("Apenas um exemplo, obrigado por visualizar");
+	}
+
+}
